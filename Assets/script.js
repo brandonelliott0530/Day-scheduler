588e830da11d827796fb8732299f16bf2edba43e
Brandon E <brandonelliott0594@gmail.com>
2022-05-21 23:22:23 -0600

    Finished functionality of the JS and created README file.

--- Assets/script.js
+++ Assets/script.js
@@ -60,6 +60,7 @@ if (hour == "8am") {
     document.getElementById("9am").classList.add("bg-secondary", "font-weight-bold")
     document.getElementById("10am").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("10am").classList.add("bg-secondary", "font-weight-bold")
+
 } else if (hour == "12pm") {
     document.getElementById("12pm").classList.add("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("12pm").classList.remove("bg-primary")
@@ -71,6 +72,7 @@ if (hour == "8am") {
     document.getElementById("10am").classList.add("bg-secondary", "font-weight-bold")
     document.getElementById("11am").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("11am").classList.add("bg-secondary", "font-weight-bold")
+
 } else if (hour == "1pm") {
     document.getElementById("1pm").classList.add("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("1pm").classList.remove("bg-primary")
@@ -84,6 +86,7 @@ if (hour == "8am") {
     document.getElementById("11am").classList.add("bg-secondary", "font-weight-bold")
     document.getElementById("12pm").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("12pm").classList.add("bg-secondary", "font-weight-bold")
+
 } else if (hour == "2pm") {
     document.getElementById("2pm").classList.add("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("2pm").classList.remove("bg-primary")
@@ -99,6 +102,7 @@ if (hour == "8am") {
     document.getElementById("12pm").classList.add("bg-secondary", "font-weight-bold")         
     document.getElementById("1pm").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("1pm").classList.add("bg-secondary", "font-weight-bold")
+
 } else if (hour == "3pm") {
     document.getElementById("3pm").classList.add("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("3pm").classList.remove("bg-primary")
@@ -116,6 +120,7 @@ if (hour == "8am") {
     document.getElementById("1pm").classList.add("bg-secondary", "font-weight-bold")
     document.getElementById("2pm").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("2pm").classList.add("bg-secondary", "font-weight-bold")
+
 } else if (hour == "4pm") {
     document.getElementById("4pm").classList.add("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("4pm").classList.remove("bg-primary")
@@ -135,6 +140,7 @@ if (hour == "8am") {
     document.getElementById("2pm").classList.add("bg-secondary", "font-weight-bold")
     document.getElementById("3pm").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("3pm").classList.add("bg-secondary", "font-weight-bold")
+
 } else if (hour == "5pm") {
     document.getElementById("5pm").classList.add("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("5pm").classList.remove("bg-primary")
@@ -156,6 +162,7 @@ if (hour == "8am") {
     document.getElementById("3pm").classList.add("bg-secondary", "font-weight-bold")
     document.getElementById("4pm").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("4pm").classList.add("bg-secondary", "font-weight-bold")
+
 } else {
     document.getElementById("5pm").classList.add("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("5pm").classList.remove("bg-primary")
@@ -179,20 +186,83 @@ if (hour == "8am") {
     document.getElementById("4pm").classList.add("bg-secondary", "font-weight-bold")
     document.getElementById("5pm").classList.remove("bg-warning", "text-dark", "font-weight-bold")
     document.getElementById("5pm").classList.add("bg-secondary", "font-weight-bold")
-
+    
 }
 console.log(hour)
 $("#time").text("Today is " + date + ". The current time is " + time)
+
+
 
 // This function is called every second
 // ====================
 
 },1000)
 
-// TODO: Set the input as JSON in localstorage, and then set it as the content on reload
+// TODO: Set the input as JSON in localstorage, and then get it as the content on reload
 // ================================================================
 
+var saveBtn = document.getElementById("save")
 
+var input8 = document.getElementById("8am")
+var input9 = document.getElementById("9am")
+var input10 = document.getElementById("10am")
+var input11 = document.getElementById("11am")
+var input12 = document.getElementById("12pm")
+var input1 = document.getElementById("1pm")
+var input2 = document.getElementById("2pm")
+var input3 = document.getElementById("3pm")
+var input4 = document.getElementById("4pm")
+var input5 = document.getElementById("5pm")
+
+saveBtn.addEventListener("click", saveItems)
+
+// This function sets the value of the input as the object in localstorage
+
+function saveItems() {
+    localStorage.setItem("8am", input8.value)
+    localStorage.setItem("9am", input9.value)
+    localStorage.setItem("10am", input10.value)
+    localStorage.setItem("11am", input11.value)
+    localStorage.setItem("12pm", input12.value)
+    localStorage.setItem("1pm", input1.value)
+    localStorage.setItem("2pm", input2.value)
+    localStorage.setItem("3pm", input3.value)
+    localStorage.setItem("4pm", input4.value)
+    localStorage.setItem("5pm", input5.value)
+    
+}
+// This function should render the value in localstorage
+// Still very repetative, but I am running out of time for the project before I need to submit it. 
+// ================================================================
+
+render()
+function render() {
+    
+    var hour8 = localStorage.getItem("8am")
+    document.getElementById("8am").value = hour8
+    var hour9 = localStorage.getItem("9am")
+    document.getElementById("9am").value = hour9
+    var hour10 = localStorage.getItem("10am")
+    document.getElementById("10am").value = hour10
+    var hour11 = localStorage.getItem("11am")
+    document.getElementById("11am").value = hour11
+    var hour12 = localStorage.getItem("12pm")
+    document.getElementById("12pm").value = hour12
+    var hour1 = localStorage.getItem("1pm")
+    document.getElementById("1pm").value = hour1
+    var hour2 = localStorage.getItem("2pm")
+    document.getElementById("2pm").value = hour2
+    var hour3 = localStorage.getItem("3pm")
+    document.getElementById("3pm").value = hour3
+    var hour4 = localStorage.getItem("4pm")
+    document.getElementById("4pm").value = hour4
+    var hour5 = localStorage.getItem("5pm")
+    document.getElementById("5pm").value = hour5
+}
+
+
+    
+    
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Day-scheduler# day-scheduler
+# Day Planner
+
+This is an example of a day planner that uses local storage to store and display the information that the user puts into the hour inputs. The current hour of the work day is highlighted as yellow in color, drawing attention to the items for that hour. The hours that are still to come, will be displayed in a dark blue color. The "Primary" class in bootstrap. The hours that have passed, will be gray in color. Methods used in this project were bootstrap, Moment.js, a small amount of jquery, JavaScript, JSON, Setinteval, and many others.
+
+## Requirements
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Usage
+
+To use the planner, the user must simply input the task that they need to remember, and press the green SAVE button on the right hand side of each row. The user will then see the information that they entered into the planner whenever they come back to view the page. Past hours will display as gray, the current hour will display as yellow, and the future hours will display as a dark blue.
+
+## Challenges
+
+The greatest challenge for me was to get the local storage to work properly. I also was unable to use a loop or something to shorten the amount of code needed to change the properties of the input fields based on the hours. I believe if I could do this again, I would do some more research into the usage of jquery, and how to change properties based on what information the moment.js is giving.
+
+## Screenshot
+
+https://user-images.githubusercontent.com/103010726/169679717-0eb7a1d5-4ef3-417c-bcad-5f3cb9b3d4ff.png
+
+
+## Github link
+
+https://github.com/brandonelliott0530/day-scheduler
+
+## Contact Me:
+Email: brandonelliott0530@gmail.com 
+Github: https://github.com/brandonelliott0530
+
